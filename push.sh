e8ee91c817ca975b1aaddbf3f62ed8c9473b3290
k123s456h <k123s456h@gmail.com>
2022-04-06 10:10:46 +0900

    T^T

--- push.sh
+++ push.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+MESSAGE_ARRAY=("8^8" ">_<" "*^*" "T^T" "OwO" "*3*")
+LENGTH=6
+INDEX="$RANDOM % $LENGTH"
+
+git config --global credential.helper store
 git add .
-git commit -m '8^8'
+git commit -m ${MESSAGE_ARRAY[$INDEX]}
 git push
